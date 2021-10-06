76229414a5b4336f96934a35d0e28807d8d2da69
vijusudhi <vjusudhi@gmail.com>
2021-10-06 12:01:39 +0200

    using csv files instead of xlsx

--- eval_cltr.py
+++ eval_cltr.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import re
+import json
 
 import os
 cwd = os.getcwd()
@@ -8,6 +9,23 @@ cwd = os.getcwd()
 print('cwd', cwd)
 path = '%s/eval.xlsx' % cwd
 
+@st.cache(allow_output_mutation=True)
+def load_df():
+    inp = pd.read_csv('input.csv', header=None, 
+                  names=['q_id', 'query_text', 'document'])
+    inp_qids = inp.q_id.to_list()
+    return inp, inp_qids
+
+@st.cache
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv(index=False, header=False).encode('utf-8')
+
+inp, inp_qids = load_df()
+log = pd.read_csv('log.csv', header=None, 
+                  names=['q_id', 'query_text', 'document', 'is_relevant', 'rel_words'])
+log_qids = log.q_id.to_list()
+to_do = [qid for qid in inp_qids if qid not in log_qids]
 
 col1, mid, col2 = st.columns([3,1,20])
 with col1:
@@ -15,51 +33,68 @@ with col1:
 with col2:
     st.write("""
     # Explainable Cross-lingual Text Retrieval on Automotive domain
-    ## Evaluation """)
-    
+    """)
+st.write("## **Evaluation of explanations**")
 
-df = pd.read_excel(path, sheet_name='eval', index_col=0)
-df['rel_words'] = df['rel_words'].astype(str)
-print(df.keys())
-
-# st.write(df)
-
-to_do = df.index[df['is_relevant'] == 'NM'].tolist()
 if len(to_do) >= 1:
     q_id = to_do[0]
 else:
     st.balloons()
+    st.write('You have evaluated all the documents. Thanks for your time! :smile:')
+    st.download_button(
+        label="Download data as CSV",
+        data=convert_df(log),
+        file_name='evaluation.csv',
+    )      
     st.stop()
     
 
-curr_df = df.loc[q_id]
+curr_df = inp[inp['q_id'] == q_id]  
 
 with st.form(key='my_form'):    
     col1, col2 = st.columns([4,20])
+    query_text = curr_df.query_text.values[0]
+    document = curr_df.document.values[0]
+    
+    col1.write('**ID**')
+    col2.write(q_id)    
     col1.write('**Query**')
-    col2.write(curr_df.query_text)
+    col2.write(query_text)
     col1.write('**Document**')
-    col2.write(curr_df.document)
+    col2.write(document)
     
     is_relevant_opt = st.radio(
         "Do you think this document is relevant?",
         ('Yes', 'No'))  
 
-    doc = curr_df.document.lower()
+    doc = document.lower()
     doc = re.sub(r'[!"#$%&\()*+/<=>?@\[\\\\\]^_`{|}~-]', '', doc)
     doc = re.sub(r'\.', '', doc)
     doc = re.sub("'", '', doc)
-    words = list(set(doc.split(' ')))
+    words = []
+    for word in doc.split(' '):
+        if word and word not in words:
+            words.append(word)
     rel_words_sel = st.multiselect(
         'Pick the relevant words',
         words,
-        [words[0], words[1]])
+        words)
     
     submit_button = st.form_submit_button(label='Submit')
     
-    if submit_button:
-        with pd.ExcelWriter(path, engine="openpyxl",
-                            mode='a+') as writer:
-            df.at[q_id,'is_relevant'] = is_relevant_opt
-            df.at[q_id,'rel_words'] = ', '.join(rel_words_sel)
-            df.to_excel(writer, sheet_name='eval')
+    if submit_button:        
+        data = {}
+        data['q_id'] = q_id
+        data['query_text'] = query_text
+        data['document'] = document
+        data['is_relevant'] = is_relevant_opt
+        data['rel_words'] = [', '.join(rel_words_sel)]
+        data = pd.DataFrame(data)
+        
+        data.to_csv('log.csv', mode='a+', index=False, header=False)
+        
+st.download_button(
+    label="Download data as CSV",
+    data=convert_df(log),
+    file_name='evaluation.csv',
+)  
